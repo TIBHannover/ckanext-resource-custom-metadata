973adb8e2951cf6f14cd419a54bb009a099f01b6
pooya.oa <pooya.oladazimi@tib.eu>
2022-01-28 09:53:36 +0100

    extend the data resource schema for adding new metadata

--- ckanext/resource_custom_metadata/plugin.py
+++ ckanext/resource_custom_metadata/plugin.py
@@ -22,9 +22,11 @@ class ResourceCustomMetadataPlugin(plugins.SingletonPlugin, toolkit.DefaultDatas
 
     def _custom_resource_schema(self, schema):
         # Add our custom_resource_text metadata field to the schema
-        schema['resources'].update({
-                'custom_resource_text' : [ toolkit.get_validator('ignore_missing') ]
-                })
+        schema['resources'].update({'material_combination' : [] })
+        schema['resources'].update({'surface_preparation' : [] })
+        schema['resources'].update({'Atmosphere' : [] })
+        schema['resources'].update({'data_type' : [] })
+        schema['resources'].update({'analysis_method' : [] })
         return schema
 
     def create_package_schema(self):
@@ -39,8 +41,9 @@ class ResourceCustomMetadataPlugin(plugins.SingletonPlugin, toolkit.DefaultDatas
 
     def show_package_schema(self):
         schema = super(ResourceCustomMetadataPlugin, self).show_package_schema()
-
-        schema['resources'].update({
-                'custom_resource_text' : [ toolkit.get_validator('ignore_missing') ]
-            })
+        schema['resources'].update({'material_combination' : [] })
+        schema['resources'].update({'surface_preparation' : [] })
+        schema['resources'].update({'Atmosphere' : [] })
+        schema['resources'].update({'data_type' : [] })
+        schema['resources'].update({'analysis_method' : [] })
         return schema
