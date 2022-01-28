d67dc9e825def8666f82477e9d95f60c1843d45b
pooya.oa <pooya.oladazimi@tib.eu>
2022-01-28 15:24:11 +0100

    added Surface Preparation field

--- ckanext/resource_custom_metadata/public/statics/add.js
+++ ckanext/resource_custom_metadata/public/statics/add.js
@@ -18,4 +18,23 @@ $(document).ready(function(){
          $(this).hide();
        }
      });
+
+     /**
+     * Add new metadata input field for surface preparation
+     * 
+     */
+      $('#surface_preparation_box_1').show();
+      $('#surface_preparation_new').click(function(){
+        let all_visible = false;
+        for(let i=1; i <= $('.surface-preparation-box').length; i++){
+          if ($('#surface_preparation_box_' + i).is(':hidden')){
+            $('#surface_preparation_box_' + i).fadeIn();
+            all_visible = true;
+            break;
+          }
+        }
+        if(!all_visible){
+          $(this).hide();
+        }
+      });
 });
