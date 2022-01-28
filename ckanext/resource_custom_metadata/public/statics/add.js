d751f10a56b772875f5f03db2215aa87370675b0
pooya.oa <pooya.oladazimi@tib.eu>
2022-01-28 16:37:49 +0100

    implemented he remove field box

--- ckanext/resource_custom_metadata/public/statics/add.js
+++ ckanext/resource_custom_metadata/public/statics/add.js
@@ -1,97 +1,32 @@
 $(document).ready(function(){
 
     /**
-     * Add new metadata input field for material combination
+     * remove a field box
      * 
      */
-     $('#material_comb_box_1').show();
-     $('#mat_comb').click(function(){
-       let all_visible = false;
-       for(let i=1; i <= $('.material-comb-box').length; i++){
-         if ($('#material_comb_box_' + i).is(':hidden')){
-           $('#material_comb_box_' + i).fadeIn();
-           all_visible = true;
-           break;
-         }
-       }
-       if(!all_visible){
-         $(this).hide();
-       }
-     });
-
-     /**
-     * Add new metadata input field for surface preparation
-     * 
-     */
-      $('#surface_preparation_box_1').show();
-      $('#surface_preparation_new').click(function(){
-        let all_visible = false;
-        for(let i=1; i <= $('.surface-preparation-box').length; i++){
-          if ($('#surface_preparation_box_' + i).is(':hidden')){
-            $('#surface_preparation_box_' + i).fadeIn();
-            all_visible = true;
-            break;
-          }
-        }
-        if(!all_visible){
-          $(this).hide();
-        }
-      });
-
-      /**
-     * Add new metadata input field for Atmosphere
-     * 
-     */
-       $('#atmosphere_box_1').show();
-       $('#atmosphere_new').click(function(){
-         let all_visible = false;
-         for(let i=1; i <= $('.atmosphere-box').length; i++){
-           if ($('#atmosphere_box_' + i).is(':hidden')){
-             $('#atmosphere_box_' + i).fadeIn();
-             all_visible = true;
-             break;
-           }
-         }
-         if(!all_visible){
-           $(this).hide();
-         }
-       });
-
-       /**
-     * Add new metadata input field for Data Type
-     * 
-     */
-        $('#data_type_box_1').show();
-        $('#data_type_new').click(function(){
-          let all_visible = false;
-          for(let i=1; i <= $('.data-type-box').length; i++){
-            if ($('#data_type_box_' + i).is(':hidden')){
-              $('#data_type_box_' + i).fadeIn();
-              all_visible = true;
-              break;
+    $('.box-remove-anchor').click(function(){
+        let id = $(this).attr('id');
+        id = id[id.length - 1];
+        field_name_box = $(this).attr('id').split('remove_')[0];
+        // hide the field box
+        $('#' + field_name_box + id).fadeOut(); 
+        
+        // unselect the checkboxes
+        field_name = $(this).attr('id').split('box_remove_')[0];
+        let checkBoxes = $('.resource-checkbox-input-' + field_name + id);
+        for(let i=0; i < checkBoxes.length; i++){
+            if($(checkBoxes[i]).prop('checked') == true){
+              $(checkBoxes[i]).click();
             }
-          }
-          if(!all_visible){
-            $(this).hide();
-          }
-        });
+        }
+        
+        // unselect the select-all 
+        if($('#select-all-resources-' + field_name + id).prop('checked') == true){
+          $('#select-all-resources-' + field_name + id).click();
+        }
 
-     /**
-     * Add new metadata input field for Analysis Method
-     * 
-     */
-      $('#analysis_method_box_1').show();
-      $('#analysis_method_new').click(function(){
-        let all_visible = false;
-        for(let i=1; i <= $('.analysis-method-box').length; i++){
-          if ($('#analysis_method_box_' + i).is(':hidden')){
-            $('#analysis_method_box_' + i).fadeIn();
-            all_visible = true;
-            break;
-          }
-        }
-        if(!all_visible){
-          $(this).hide();
-        }
-      });
+        $('#' + field_name + id).val('');
+        
+    });
+
 });
---
+++ ckanext/resource_custom_metadata/public/statics/field.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+    /**
+     * Add new metadata input field for material combination
+     * 
+     */
+     $('#material_combination_box_1').show();
+     $('#mat_comb').click(function(){
+       let all_visible = false;
+       for(let i=1; i <= $('.material-comb-box').length; i++){
+         if ($('#material_combination_box_' + i).is(':hidden')){
+           $('#material_combination_box_' + i).fadeIn();
+           all_visible = true;
+           break;
+         }
+       }
+       if(!all_visible){
+         $(this).hide();
+       }
+     });
+
+     /**
+     * Add new metadata input field for surface preparation
+     * 
+     */
+      $('#surface_preparation_box_1').show();
+      $('#surface_preparation_new').click(function(){
+        let all_visible = false;
+        for(let i=1; i <= $('.surface-preparation-box').length; i++){
+          if ($('#surface_preparation_box_' + i).is(':hidden')){
+            $('#surface_preparation_box_' + i).fadeIn();
+            all_visible = true;
+            break;
+          }
+        }
+        if(!all_visible){
+          $(this).hide();
+        }
+      });
+
+      /**
+     * Add new metadata input field for Atmosphere
+     * 
+     */
+       $('#atmosphere_box_1').show();
+       $('#atmosphere_new').click(function(){
+         let all_visible = false;
+         for(let i=1; i <= $('.atmosphere-box').length; i++){
+           if ($('#atmosphere_box_' + i).is(':hidden')){
+             $('#atmosphere_box_' + i).fadeIn();
+             all_visible = true;
+             break;
+           }
+         }
+         if(!all_visible){
+           $(this).hide();
+         }
+       });
+
+       /**
+     * Add new metadata input field for Data Type
+     * 
+     */
+        $('#data_type_box_1').show();
+        $('#data_type_new').click(function(){
+          let all_visible = false;
+          for(let i=1; i <= $('.data-type-box').length; i++){
+            if ($('#data_type_box_' + i).is(':hidden')){
+              $('#data_type_box_' + i).fadeIn();
+              all_visible = true;
+              break;
+            }
+          }
+          if(!all_visible){
+            $(this).hide();
+          }
+        });
+
+     /**
+     * Add new metadata input field for Analysis Method
+     * 
+     */
+      $('#analysis_method_box_1').show();
+      $('#analysis_method_new').click(function(){
+        let all_visible = false;
+        for(let i=1; i <= $('.analysis-method-box').length; i++){
+          if ($('#analysis_method_box_' + i).is(':hidden')){
+            $('#analysis_method_box_' + i).fadeIn();
+            all_visible = true;
+            break;
+          }
+        }
+        if(!all_visible){
+          $(this).hide();
+        }
+      });
+});
