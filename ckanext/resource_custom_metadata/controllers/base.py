92351e55004bb42b04d83e9dc60cf3e24362f7df
pooya.oa <pooya.oladazimi@tib.eu>
2022-02-02 16:59:10 +0100

    added url redirection after saving

--- ckanext/resource_custom_metadata/controllers/base.py
+++ ckanext/resource_custom_metadata/controllers/base.py
@@ -20,19 +20,29 @@ class BaseController():
     def save_metadata():
         metadata_fields = ['material_combination', 'surface_preparation', 'atmosphere', 'data_type', 'analysis_method']
         resource_count = request.form.get('resources_count')
+        package_name = request.form.get('pkg_name')
         
-        for field in metadata_fields:
-            for i in range(1, int(resource_count) + 1):
-                resource_ids = request.form.getlist('custom_metadata_' + field + '_' + str(i))
-                field_text = request.form.get(field + '_' + str(i))
+        try:
+            for field in metadata_fields:
+                for i in range(1, int(resource_count) + 1):
+                    resource_ids = request.form.getlist('custom_metadata_' + field + '_' + str(i))
+                    field_text = request.form.get(field + '_' + str(i))
 
-                for res_id in resource_ids:
-                    resource = toolkit.get_action('resource_show')({}, {'id': res_id})
-                    resource[field] = field_text
-                    toolkit.get_action('resource_update')({}, resource)
+                    for res_id in resource_ids:
+                        resource = toolkit.get_action('resource_show')({}, {'id': res_id})
+                        resource[field] = field_text
+                        toolkit.get_action('resource_update')({}, resource)
+        
+        except:
+            return '0'
 
 
+        if Helper.is_plugin_enabled("organization_group"): # if organization_group plugin exists:
+            return redirect(h.url_for('organization_group.add_ownership_view', id=str(package_name) ,  _external=True)) 
 
+        elif Helper.is_plugin_enabled("media_wiki"): # if media_wiki plugin exists
+            return redirect(h.url_for('media_wiki.machines_view', id=str(package_name) ,  _external=True)) 
 
-        return '0'
+        return redirect(h.url_for('dataset.read', id=str(package_name) ,  _external=True)) 
+        
         
