604d91e5e0873a36f991ed03288d9aa059331b5e
pooya.oa <pooya.oladazimi@tib.eu>
2022-02-02 13:57:15 +0100

    fix the issue with hiding already selected checkbox

--- ckanext/resource_custom_metadata/public/statics/add.js
+++ ckanext/resource_custom_metadata/public/statics/add.js
@@ -53,23 +53,31 @@ $(document).ready(function(){
      * 
      */
 
-      let resources =  $('.resource-box');
-      for(let i=0; i < resources.length; i++){
-        if($(resources[i]).prop('checked') == true){
-          $(".checkbox-container[value=" + $(resources[i]).val() + "]").hide();
-          $(resources[i]).parent().show();  
-        }
-      }
+      // let resources =  $('.resource-box');
+      // for(let i=0; i < resources.length; i++){
+      //   if($(resources[i]).prop('checked') == true){
+      //     $(".checkbox-container[value=" + $(resources[i]).val() + "]").hide();
+      //     $(resources[i]).parent().show();  
+      //   }
+      // }
   
       $('.resource-box').click(function(){
-          let resource = $(this).val();
+          let resources_checkbox = $('.resource-box');
+          for(let i=0; i < resources_checkbox.length; i++){
+              if($(resources_checkbox[i]).val() ===  $(this).val() && $(resources_checkbox[i]).attr('field_name') ===  $(this).attr('field_name')){
+                if($(this).prop('checked') == true){
+                  $(resources_checkbox[i]).parent().hide();    
+                }
+                else{
+                  $(resources_checkbox[i]).parent().show();
+                }
+              }
+          }
+
           if($(this).prop('checked') == true){
-              $(".checkbox-container[value=" + resource + "]").hide();
-              $(this).parent().show();          
+            $(this).parent().show();          
           }
-          else{
-            $(".checkbox-container[value=" + resource + "]").show();
-          }
+          
       });
 
 });
