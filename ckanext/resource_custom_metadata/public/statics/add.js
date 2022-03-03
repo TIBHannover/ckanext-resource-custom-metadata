63c8e67fca2a94d7362e08e84e36a2c28cf05d8a
pooya.oa <pooya.oladazimi@tib.eu>
2022-03-03 12:01:27 +0100

    hide already selected resources from other modals

--- ckanext/resource_custom_metadata/public/statics/add.js
+++ ckanext/resource_custom_metadata/public/statics/add.js
@@ -49,26 +49,22 @@ $(document).ready(function(){
 
 
      /**
-     * hide a resource from other modals when the resource is chosen for one machine in a modal
+     * hide a resource from other modals when the resource is chosen for one metadata in a modal
      * 
      */
 
-      let resources_checkbox = $('.resource-box');
-      for(let i=0; i < resources_checkbox.length; i++){
-          if($(resources_checkbox[i]).val() ===  $(this).val() && $(resources_checkbox[i]).attr('field_name') ===  $(this).attr('field_name')){
-            if($(this).prop('checked') == true){
-              $(resources_checkbox[i]).parent().hide();    
-            }
-            else{
-              $(resources_checkbox[i]).parent().show();
-            }
-          }
+      let resources =  $('.resource-box');
+      for(let i=0; i < resources.length; i++){
+        if($(resources[i]).prop('checked') == true){
+          $(".resource-box[value=" + $(resources[i]).val() + "]").parent().hide();
+        }
+      }
+      for(let i=0; i < resources.length; i++){
+        if($(resources[i]).prop('checked') == true){          
+          $(resources[i]).parent().show();  
+        }
       }
 
-      if($(this).prop('checked') == true){
-        $(this).parent().show();          
-      }
-  
       $('.resource-box').click(function(){
           let resources_checkbox = $('.resource-box');
           for(let i=0; i < resources_checkbox.length; i++){
