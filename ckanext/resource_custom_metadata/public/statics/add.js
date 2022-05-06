05c07228a26db489b5c7667fd74b6f26409191a2
pooya.oa <pooya.oladazimi@tib.eu>
2022-05-06 13:00:00 +0200

    detect the non selected resources for a metadata field

--- ckanext/resource_custom_metadata/public/statics/add.js
+++ ckanext/resource_custom_metadata/public/statics/add.js
@@ -19,6 +19,35 @@ $(document).ready(function(){
      */
     $('#resource-custom-metadata-form').submit(function(e){
       e.preventDefault();
+      $('#warning_result_box').html('')
+      let resourcesCount = $('#resource_count').val();
+      let metadataFieldsIds = ['material_combination_', 'surface_preparation_', 'atmosphere_', 'data_type_', 'analysis_method_'];
+      let metadataFields = ['Material or Material Combination', 'Surface Preparation', 'Atmosphere', 'Data type', 'Measurement/Analysis Method'];
+      let prefix = 'resource-checkbox-input-';
+      for(let i=0; i < metadataFieldsIds.length; i++){
+        $('#warning_result_box').append("<strong>" +  metadataFields[i] + "</strong>");
+        $('#warning_result_box').append('<br>');
+        let resources_for_this_metadata = $('.' + prefix + metadataFieldsIds[i]);
+        let resource_name = "";  
+        let already_seen_resources = [];
+        let selected_resources = [];    
+        for(let m=0; m < resources_for_this_metadata.length; m++){
+          resource_name = $(resources_for_this_metadata[m]).attr("resource_name");
+          if(!already_seen_resources.includes(resource_name)){
+            already_seen_resources.push(resource_name);
+          }      
+          if($(resources_for_this_metadata[m]).prop('checked') == true){
+            selected_resources.push(resource_name);          
+          }
+        }        
+        for(let n=0; n < already_seen_resources.length; n++){
+          if(!selected_resources.includes(already_seen_resources[n])){
+            $('#warning_result_box').append(already_seen_resources[n]);
+            $('#warning_result_box').append('<br>');
+          }
+        }        
+      }
+    
       $('#resource_metadata_warning').modal('show');
 
     });
